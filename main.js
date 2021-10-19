f03532509ee24a29a9fcc7d53c5291f951db2b60
mam159 <mariano.massa78@gmail.com>
2021-10-19 20:19:07 -0300

    se suben cambios 19/10

---
+++ main.js
@@ -0,0 +1,133 @@
+
+//--------------------------Ejercicio 1------------------------------------------------
+
+// let nombre = "Mariano"
+// let apellido = "Massa"
+// let comision = "1112"
+// let hobbies = ["Jugar al Futbol","Andar en Bicicleta"]
+
+// console.log(nombre + "-" + apellido + "-" + comision + "-" + hobbies)
+
+
+//--------------------------Ejercicio 2------------------------------------------------
+
+//let frase = prompt("Ingrese una frase");
+//console.log(frase)
+
+
+//--------------------------Ejercicio 3------------------------------------------------
+
+//let nombre = prompt("Ingrese su nombre");
+//let imprimir = "Hola " + nombre + "!"
+//console.log(imprimir)
+
+//--------------------------Ejercicio 4------------------------------------------------
+
+//let nombre = prompt("Ingrese su nombre");
+//let apellido = prompt("Ingrese su apellido");
+//let imprimir = "Hola " + nombre + " "+ apellido + "!"
+//console.log(imprimir)
+
+//--------------------------Ejercicio 5------------------------------------------------
+
+//let edad = prompt("Ingrese su edad");
+//let imprimir = "Tenes " + edad + " años"
+//console.log(imprimir)
+
+//--------------------------Ejercicio 6------------------------------------------------
+
+//let peso = prompt("Ingrese su peso");
+//let altura = prompt("Ingrese su altura");
+//let imc = peso / (altura^2)
+//let mensaje = "Su IMC es igual a " + imc
+//console.log(mensaje)
+
+//--------------------------Ejercicio 7------------------------------------------------
+
+//let cantidadDeVentanas = 8
+//console.log(cantidadDeVentanas)
+
+//--------------------------Ejercicio 8------------------------------------------------
+
+//let soyHumano = true
+//console.log(soyHumano)
+
+//--------------------------Ejercicio 9------------------------------------------------
+
+//let miGustoDePizza = "Jamon y Morron"
+//console.log(miGustoDePizza)
+
+//--------------------------Ejercicio 10------------------------------------------------
+
+//let mostrar = false
+//console.log(mostrar)
+
+//mostrar = "Hola"
+//console.log(mostrar)
+
+//mostrar = 0
+//console.log(mostrar)
+
+//mostrar = "Minions"
+//console.log(mostrar)
+
+//--------------------------Ejercicio 11------------------------------------------------
+
+//function calcularHipotenusa(valor1, valor2){
+//    return Math.sqrt(Math.pow(valor1,2)+Math.pow(valor2,2)) 
+//}
+
+//let base = Number(prompt("Ingrese la base"))
+//let altura = Number(prompt("Ingrese la altura"))
+//let superficie = (base*altura)/2
+//let perimetro = base + altura + calcularHipotenusa(base, altura)
+
+//console.log("La superficie o area es de " + superficie )
+//console.log("La perimetro es de " + perimetro )
+
+//--------------------------Ejercicio 12------------------------------------------------
+
+//function librasAKg(valor1){
+//    return valor1 * 0.45359237
+//}
+
+//let libras = Number(prompt("Ingrese su peso en libras"))
+
+//console.log("Su peso en kg es de " + librasAKg(libras) )
+
+//--------------------------Ejercicio 13------------------------------------------------
+
+function calculaAutmento(valor1){
+    //return Math.round(valor1 * 1.15 * 100)/100
+    return valor1 * 1.15
+}
+
+let sueldo = parseFloat(prompt("Ingrese su sueldo"))
+
+console.log("Su sueldo actualizado es de " + calculaAutmento(sueldo) )
+
+//--------------------------Ejercicio 14------------------------------------------------
+
+//function resultado(valor1){
+//    if (valor1 > 7 ) {
+//        return "Aprobado"
+//    } else {
+//        return "Reprobado"
+//    }
+//}
+
+//let nota = parseFloat(prompt("Ingrese su nota"))
+
+//alert(resultado(nota))
+
+//--------------------------Ejercicio 15------------------------------------------------
+
+//Siguiente tabla?
+
+//--------------------------Ejercicio 16------------------------------------------------
+
+//Matricula?
+
+//--------------------------Ejercicio 17------------------------------------------------
+
+
